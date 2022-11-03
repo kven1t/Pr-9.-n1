61d0f25b9534113d0eb0dba409d0420de3e6c376
hp <hp@HP>
2022-11-03 22:43:50 +0300

    Добавьте файлы проекта.

---
+++ Pr 9. n1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace Pr_9._n1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Курс доллара к рублю: ");
+            double kyrs = double.Parse(Console.ReadLine());
+            Console.Write("\n");
+            for (int i = 5; i <= 500; i += 5)
+            {
+                Console.WriteLine($"{i}$ - {i * kyrs}Р");
+            }
+        }
+    }
+}
